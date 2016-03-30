b122191b21c4a3038157644b13b556b6b8793978
rawk-v <huangruhui.v@gmail.com>
2016-03-30 09:20:34 +0800

    Add comment support.

--- bog/forms.py
+++ bog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-from .models import Post
+from .models import Post,Comment
 
 
 class PostForm(forms.ModelForm):
@@ -9,3 +9,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
--- bog/views.py
+++ bog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -58,3 +58,31 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('bog.views.post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('bog.views.post_detail', pk=post.pk)
+        else:
+            form = CommentForm()
+    else:
+        form = CommentForm()
+    return render(request, 'bog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('bog.views.post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('bog.views.post_detail', pk=post_pk)
